021b11149339586484e0b593854cf0cfe00df39b
Oshgnacknak <osh@oshgnacknak.de>
2022-09-15 13:51:23 +0200

    Add missing JavaDoc

--- src/main/java/h11/parse/Projection.java
+++ src/main/java/h11/parse/Projection.java
@@ -30,6 +30,12 @@ public record Projection(char source, String destination) {
         this(getFirstChar(source), destination);
     }
 
+    /**
+     * Assure the given source String hat length one
+     * and return its first character.
+     * @param source The source String.
+     * @return The first character of the given source String.
+     */
     private static char getFirstChar(String source) {
         if (source.length() != 1) {
             throw new IllegalArgumentException("Source String must be exactly 1 character long: " + source);
@@ -37,6 +43,10 @@ public record Projection(char source, String destination) {
         return source.charAt(0);
     }
 
+    /**
+     * Assure the given destination String is not empty.
+     * @param destination The destination String.
+     */
     private void checkDestinationNotEmpty(String destination) {
         if (destination.isEmpty()) {
             throw new IllegalArgumentException("Destination String cannot be empty");
